177ba080d82f076b0357d5e68c7d63d2acf67166
martinondiwa <surekondiwa@gmail.com>
2025-05-15 00:23:21 +0000

    asd

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,52 +1,33 @@
-#!/usr/bin/env fish
-python3 -m doctest -v (basename (status -f))
-exit
+#!/usr/bin/env python3
+"""First-In First-Out caching module.
 """
->>> FIFOCache = __import__('1-fifo_cache').FIFOCache
->>> my_cache = FIFOCache()
->>> my_cache.put("A", "Hello")
->>> my_cache.put("B", "World")
->>> my_cache.put("C", "Holberton")
->>> my_cache.put("D", "School")
->>> my_cache.print_cache()
-Current cache:
-A: Hello
-B: World
-C: Holberton
-D: School
+from collections import OrderedDict
 
->>> my_cache.put("E", "Battery")
-DISCARD: A
+from base_caching import BaseCaching
 
->>> my_cache.print_cache()
-Current cache:
-B: World
-C: Holberton
-D: School
-E: Battery
 
->>> my_cache.put("C", "Street")
->>> my_cache.print_cache()
-Current cache:
-B: World
-C: Street
-D: School
-E: Battery
+class FIFOCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
+    """
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
 
->>> my_cache.put("F", "Mission")
-DISCARD: B
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
->>> my_cache.print_cache()
-Current cache:
-C: Street
-D: School
-E: Battery
-F: Mission
-
->>> print(my_cache.get(None))
-None
-
->>> print(my_cache.get("microsoft_build"))
-None
-
-"""
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
